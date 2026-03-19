889e7b47ba2911d5ae73209f618152e14d69325e
mtheli <martin.thelian@gmx.at>
2026-03-19 22:28:46 +0100

    Bump ESP bridge version to 1.1.0

--- esphome/components/philips_sonicare/philips_sonicare.h
+++ esphome/components/philips_sonicare/philips_sonicare.h
@@ -14,7 +14,7 @@
 namespace esphome {
 namespace philips_sonicare {
 
-static const char *const PHILIPS_SONICARE_VERSION = "1.0.0";
+static const char *const PHILIPS_SONICARE_VERSION = "1.1.0";
 
 class PhilipsSonicare : public ble_client::BLEClientNode,
                         public Component,
